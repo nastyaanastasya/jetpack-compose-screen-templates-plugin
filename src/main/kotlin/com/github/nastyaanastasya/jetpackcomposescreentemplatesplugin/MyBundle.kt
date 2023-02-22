9bf76e970dd5215ac5188696354d4779c44cc967
GitHub Action <action@github.com>
2023-02-22 19:09:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/nastyaanastasya/jetpackcomposescreentemplatesplugin/MyBundle.kt
+++ src/main/kotlin/com/github/nastyaanastasya/jetpackcomposescreentemplatesplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nastyaanastasya.jetpackcomposescreentemplatesplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
